575e264dc8bdbcbc191d5613401c28247b7f695e
ferro96 <davidac099@gmail.com>
2024-09-06 21:42:26 -0600

    Remove previous many to many logic and update it to make it cleaner

--- db/migrate/20240906043436_create_join_table_player_hero.rb
+++
@@ -1,9 +0,0 @@
-class CreateJoinTablePlayerHero < ActiveRecord::Migration[7.2]
-  def change
-    create_table :player_hero do |t|
-      t.belongs_to :player
-      t.belongs_to :hero
-      t.timestamps
-    end
-  end
-end
